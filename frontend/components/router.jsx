0ef89b92fae67208df8ebff002db8db16091edc5
Calvin <calvinhlee23@gmail.com>
2016-09-27 21:22:08 -0700

    done with session/auth setup

--- frontend/components/router.jsx
+++ frontend/components/router.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 import App from  './app';
 import SessionFormContainer from './session/session_form_container';
-import GreetingContainer from './greeting/greeting_container';
+import StaticPageContainer from './static_page/static_page_container';
 
 const AppRouter = () => (
   <Router history = {hashHistory}>
     <Route path ="/" component = {App}>
-    <IndexRoute component = {GreetingContainer}/>
+    <IndexRoute component = {StaticPageContainer}/>
       <Route path = "signup" component = {SessionFormContainer}/>
       <Route path = "login" component = {SessionFormContainer}/>
       <Route path = "logout" component = {SessionFormContainer}/>
