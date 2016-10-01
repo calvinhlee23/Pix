385dc76b66f44b467f56cfec67feb8aeb1de75ca
Calvin <calvinhlee23@gmail.com>
2016-09-30 19:36:51 -0700

    testing

--- frontend/components/stream/frame.jsx
+++ frontend/components/stream/frame.jsx
@@ -3,12 +3,12 @@ import React from  'react';
 class Frame extends React.Component {
   constructor (props) {
     super(props);
+    console.log(this.props.image);
   }
 
   render() {
     return(
       <li key = {this.props.image.id} className = "Frame">
-        {console.log(this.props.image)}
         <h1>{this.props.image.user.user_name}</h1>
         <img src = {this.props.image.cloud_url} id = "image"/>
         <div>{this.props.image.created_at}</div>
