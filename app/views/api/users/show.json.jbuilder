cd1e22871efb4be0cb62cfda6b2e2a7d20d1206b
Calvin <calvinhlee23@gmail.com>
2016-10-13 19:44:35 -0700

    bug fixes

--- app/views/api/users/show.json.jbuilder
+++ app/views/api/users/show.json.jbuilder
@@ -2,6 +2,6 @@ json.user do
   json.id @user.id
   json.public @user.public
   json.email @user.email
-  json.user_name @user.user_name, :user_name
+  json.user_name @user.user_name
   json.following @user.following_users, :user_name
 end
--- frontend/components/router.jsx
+++ frontend/components/router.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import {Router, Route, IndexRoute, useRouterHistory} from 'react-router';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 import { createHashHistory } from 'history';
 import App from  './app';
 import SessionFormContainer from './session/session_form_container';
 import StaticPageContainer from './static_page/static_page_container';
-
-const hashHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 const AppRouter = () => (
   <Router history = {hashHistory}>
