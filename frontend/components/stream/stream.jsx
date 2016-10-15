021dd5c6743ad82b56f6a6a8b5908b168f9ba06e
Calvin <calvinhlee23@gmail.com>
2016-10-15 16:32:37 -0700

    bug fixes

--- frontend/components/stream/stream.jsx
+++ frontend/components/stream/stream.jsx
@@ -9,14 +9,6 @@ class Stream extends React.Component {
     };
   }
 
-  componentWillReceiveProps() {
-    var path = this.props.location.pathname.split("/");
-    if (path[1] === "user") {
-      this.setState({path: path});
-      var userName = path[2];
-      this.props.requestImages("userImages", userName);
-    }
-  }
   componentDidMount() {
     var path = this.props.location.pathname.split("/");
     if (path[1] === "user") {
