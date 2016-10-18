3799b848d545f4bd5581252e30488ce00d7a0973
Calvin <calvinhlee23@gmail.com>
2016-10-17 18:09:54 -0700

    time ago parsed at jbuilder lvl

--- app/views/api/images/_requestedImages.json.jbuilder
+++ app/views/api/images/_requestedImages.json.jbuilder
@@ -3,7 +3,7 @@
      json.id img.id
      json.user img.user
      json.cloud_url img.cloud_url
-     json.created_at img.created_at
+     json.created_at time_ago_in_words(img.created_at)
      json.likes img.likes, :author_id
      json.comments img.comments
    end
--- frontend/components/stream/frame.jsx
+++ frontend/components/stream/frame.jsx
@@ -30,33 +30,6 @@ class Frame extends React.Component {
     this.setState({commentBody: body});
   }
 
-  timeSince(date) {
-      var seconds = Math.floor((new Date() - date) / 1000);
-
-      var interval = Math.floor(seconds / 31536000);
-
-      if (interval > 1) {
-          return interval + " years";
-      }
-      interval = Math.floor(seconds / 2592000);
-      if (interval > 1) {
-          return interval + " months";
-      }
-      interval = Math.floor(seconds / 86400);
-      if (interval > 1) {
-          return interval + " days";
-      }
-      interval = Math.floor(seconds / 3600);
-      if (interval > 1) {
-          return interval + " hours";
-      }
-      interval = Math.floor(seconds / 60);
-      if (interval > 1) {
-          return interval + " minutes";
-      }
-      return Math.floor(seconds) + " seconds";
-  }
-
   render() {
     var userName = this.props.image.user.user_name;
     return(
@@ -69,8 +42,7 @@ class Frame extends React.Component {
                       deleteThis = {this.props.deleteThis}
                       currentUser = {this.props.currentUser}/>
 
-        <div className = "time-ago">
-          {this.timeSince(Date.parse(this.props.image.created_at))} ago</div>
+        <div className = "time-ago">{this.props.image.created_at} ago</div>
 
 
         <Like {...this.props}/>
