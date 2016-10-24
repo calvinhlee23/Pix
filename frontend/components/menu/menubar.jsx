509bffb52c9d4a9392724ba7671136f32181d675
Calvin <calvinhlee23@gmail.com>
2016-10-23 17:53:45 -0700

    disabled browser horizontal slide

--- frontend/components/menu/menubar.jsx
+++ frontend/components/menu/menubar.jsx
@@ -22,22 +22,26 @@ class MenuBar extends React.Component {
 
   render() {
     return(
-      <div className = "menu-bar">
-        <div id = "user">
-        <h2>Hello {this.props.currentUser.user_name}</h2>
-        <button onClick = {this.toLogOut.bind(this)}>Logout</button></div>
-        <ul className = "stream-menu">
-          <li id = "following-stream-menu"
-          onClick = {this.ownRequestImages("followingImages")}>
-          Following Stream</li>
-          <li id = "public-stream-menu"
-          onClick = {this.ownRequestImages("publicImages")}>Public Stream</li>
-          <li id = "my-stream-menu"
-          onClick = {this.ownRequestImages("myImages")}>My Stream</li>
-          <li id = "upload-button"><UploadButton id = "upload-button"
-          postImage = {this.props.postImage.bind(this)}/></li>
-          <li><UserSearch className = "user-search"/></li>
-        </ul>
+      <div>
+        <div className = "menu-bar">
+          <div id = "user">
+          <h2>Hello {this.props.currentUser.user_name}</h2>
+          <button onClick = {this.toLogOut.bind(this)}>Logout</button></div>
+        </div>
+        <aside className = "stream-menu-wrapper">
+          <ul className = "stream-menu">
+            <li id = "following-stream-menu"
+            onClick = {this.ownRequestImages("followingImages")}>
+            Following Stream</li>
+            <li id = "public-stream-menu"
+            onClick = {this.ownRequestImages("publicImages")}>Public Stream</li>
+            <li id = "my-stream-menu"
+            onClick = {this.ownRequestImages("myImages")}>My Stream</li>
+            <li id = "upload-button"><UploadButton id = "upload-button"
+            postImage = {this.props.postImage.bind(this)}/></li>
+            <li><UserSearch className = "user-search"/></li>
+          </ul>
+        </aside>
       </div>
     );
   }
