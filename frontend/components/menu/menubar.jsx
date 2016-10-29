c1474e7a223eb5b8e80a67bd8c4b56b30571b617
Calvin <calvinhlee23@gmail.com>
2016-10-28 21:43:02 -0700

    bug fixes

--- frontend/components/menu/menubar.jsx
+++ frontend/components/menu/menubar.jsx
@@ -8,6 +8,9 @@ import UserProfile from '../user_profile/user_profile';
 class MenuBar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      loading: false
+    };
   }
 
   componentDidMount() {
@@ -15,19 +18,19 @@ class MenuBar extends React.Component {
       if (path[1] === "user") {
         var userName = path[2];
         this.props.requestTargetUser(userName);
+        this.setState({loading: true});
     }
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.location.pathname !== this.props.location.pathname) {
       if (nextProps.location.pathname.split("/")[1] === "user") {
-        this.setState({loading: true});
         var path = nextProps.location.pathname.split("/");
         var userName = path[2];
         this.props.requestTargetUser(userName);
-        setTimeout(() => {
-          this.setState({loading: false});
-        }, 300);
+        this.setState({loading: true});
+      } else {
+        this.setState({loading: false});
       }
     }
   }
@@ -48,35 +51,66 @@ class MenuBar extends React.Component {
   }
 
   render() {
-    return(
-      <div>
+    if (this.state.loading) {
+      return(
+        <div>
         <div className = "menu-bar">
-          <div id = "user">
-          <div className = "glyphicon glyphicon-home"
-                onClick = {this.toHome.bind(this)}
-                id = "home"/>
-          <h2>Hello {this.props.currentUser.user_name}</h2>
-          <button onClick = {this.toLogOut.bind(this)}>Logout</button></div>
-          <UserProfile {...this.props}/>
+        <div id = "user">
+        <div className = "glyphicon glyphicon-home"
+        onClick = {this.toHome.bind(this)}
+        id = "home"/>
+        <h2>Hello {this.props.currentUser.user_name}</h2>
+        <button onClick = {this.toLogOut.bind(this)}>Logout</button></div>
+        <UserProfile {...this.props}/>
         </div>
 
         <aside className = "stream-menu-wrapper">
         <Gagets/>
-          <ul className = "stream-menu">
-            <li id = "following-stream-menu"
-            onClick = {this.ownRequestImages("followingImages")}>
-            Following Stream</li>
-            <li id = "public-stream-menu"
-            onClick = {this.ownRequestImages("publicImages")}>Public Stream</li>
-            <li id = "my-stream-menu"
-            onClick = {this.ownRequestImages("myImages")}>My Stream</li>
-            <li id = "upload-button"><UploadButton id = "upload-button"
-            postImage = {this.props.postImage.bind(this)}/></li>
-            <UserSearch className = "user-search"/>
-          </ul>
+        <ul className = "stream-menu">
+        <li id = "following-stream-menu"
+        onClick = {this.ownRequestImages("followingImages")}>
+        Following Stream</li>
+        <li id = "public-stream-menu"
+        onClick = {this.ownRequestImages("publicImages")}>Public Stream</li>
+        <li id = "my-stream-menu"
+        onClick = {this.ownRequestImages("myImages")}>My Stream</li>
+        <li id = "upload-button"><UploadButton id = "upload-button"
+        postImage = {this.props.postImage.bind(this)}/></li>
+        <UserSearch className = "user-search"/>
+        </ul>
         </aside>
-      </div>
-    );
+        </div>
+      );
+    } else {
+      return(
+        <div>
+        <div className = "menu-bar">
+        <div id = "user">
+        <div className = "glyphicon glyphicon-home"
+        onClick = {this.toHome.bind(this)}
+        id = "home"/>
+        <h2>Hello {this.props.currentUser.user_name}</h2>
+        <button onClick = {this.toLogOut.bind(this)}>Logout</button></div>
+        </div>
+
+        <aside className = "stream-menu-wrapper">
+        <Gagets/>
+        <ul className = "stream-menu">
+        <li id = "following-stream-menu"
+        onClick = {this.ownRequestImages("followingImages")}>
+        Following Stream</li>
+        <li id = "public-stream-menu"
+        onClick = {this.ownRequestImages("publicImages")}>Public Stream</li>
+        <li id = "my-stream-menu"
+        onClick = {this.ownRequestImages("myImages")}>My Stream</li>
+        <li id = "upload-button"><UploadButton id = "upload-button"
+        postImage = {this.props.postImage.bind(this)}/></li>
+        <UserSearch className = "user-search"/>
+        </ul>
+        </aside>
+        </div>
+      );
+    }
   }
 }
 
