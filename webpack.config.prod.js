57415fe8f6e881e1a48c7f881be492638f871c2f
Calvin <calvinhlee23@gmail.com>
2016-10-31 21:55:38 -0700

    bug fixes

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,7 @@ var webpack = require("webpack");
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/pix.jsx',
+  entry: './frontend/pix.jsx',  
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
