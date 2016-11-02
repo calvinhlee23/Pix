fb2de1f01ee2960345664cfe592a5497a8bb40d5
Calvin <calvinhlee23@gmail.com>
2016-11-01 21:16:35 -0700

    added loader to stream

--- frontend/components/stream/stream.jsx
+++ frontend/components/stream/stream.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Frame from './frame';
 import {hashHistory} from 'react-router';
+import Loader from '../../util/loader';
+
 class Stream extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {
+      needLoader: true
+    };
   }
 
   componentWillReceiveProps(nextProps) {
+    this.setState({needLoader: true});
     if (nextProps.location.pathname !== this.props.location.pathname) {
       var path = nextProps.location.pathname.split("/");
       if (path[1] === "user") {
@@ -18,9 +24,13 @@ class Stream extends React.Component {
         this.props.requestImages(path[1]);
       }
     }
+    setTimeout(() => {
+      this.setState({needLoader:false});
+    }, 1500);
   }
 
   componentDidMount() {
+    this.setState({needLoader: true});
     var path = this.props.location.pathname.split("/");
     if (path[1] === "user") {
       var userName = path[2];
@@ -30,24 +40,33 @@ class Stream extends React.Component {
                path[1] === "publicImages") {
       this.props.requestImages(path[1]);
     }
+    setTimeout(() => {
+      this.setState({needLoader:false});
+    }, 1500);
   }
 
   render() {
     // infinite loades 3 rows of frames per.
-    return (
-      <div className = "stream">
-        <ul className = "stream-frame">
-        {Object.keys(this.props.images).reverse().map((imgId)=> {
-          return <Frame image = {this.props.images[imgId]}
-                        currentUser = {this.props.currentUser}
-                        postComment = {this.props.postComment}
-                        deleteThis = {this.props.deleteThis.bind(this)}
-                        processLike = {this.props.processLike}
-                        key = {imgId}/>;
-        })}
-        </ul>
-      </div>
-    );
+    if (this.state.needLoader) {
+      return (
+        <Loader className = "stream-loader"/>
+      );
+    } else {
+      return (
+        <div className = "stream">
+          <ul className = "stream-frame">
+          {Object.keys(this.props.images).reverse().map((imgId)=> {
+            return <Frame image = {this.props.images[imgId]}
+            currentUser = {this.props.currentUser}
+            postComment = {this.props.postComment}
+            deleteThis = {this.props.deleteThis.bind(this)}
+            processLike = {this.props.processLike}
+            key = {imgId}/>;
+          })}
+          </ul>
+        </div>
+      );
+    }
   }
 }
 
