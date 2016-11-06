402e09a97d93dec468825dc606e6e0cef81d47f1
Calvin <calvinhlee23@gmail.com>
2016-11-05 19:03:37 -0700

    finished seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-a = User.create(email: "calvinhlee23@gmail.com", user_name: "hello_calvin",
+a = User.create(email: "calvinhlee23@gmail.com", user_name: "Calvinni",
                 password: "password")
 
-img1 = Image.create(user_id: 1, cloud_url: "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1477443118/mednpqfwqiuwui7rk3g5.jpg")
-img2 = Image.create(user_id: 1, cloud_url: "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1477443126/vtumrqdn4nlxeizn8nir.jpg")
-img3 = Image.create(user_id: 1, cloud_url: "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1477443108/iltv10zk9xhwwq2t3sn0.jpg")
-
 guest = User.create(email: "guest@guest.com", user_name: "Guest", password: "guest_password")
-
+img1 = Image.create(user_id: 1, cloud_url: "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1477443118/cioax7bqmtdualwqowfr.jpg")
+img1 = Image.create(user_id: 1, cloud_url: "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1477443126/yks7te7sw1dm6xulux7k.jpg")
+img1 = Image.create(user_id: 1, cloud_url: "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/ayzf3isfatw7dyhh2ide.jpg")
 comment1 = Comment.create(
   author_name: User.first.user_name,
   author_id: 1, image_id: img1.id,
@@ -36,10 +34,25 @@ end
 end
 
 #create images
-200.times do
-  imgLinks = ["http://res.cloudinary.com/dxgn2rpkt/image/upload/v1477443118/cioax7bqmtdualwqowfr.jpg",
-              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1477443126/yks7te7sw1dm6xulux7k.jpg",
-              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/ayzf3isfatw7dyhh2ide.jpg",
+150.times do
+  imgLinks = [
+              # actualphotos
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/u201ywuumn53c4h6xczu.jpg",
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/esat9pzosseqhwogzrxq.jpg",
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/fmtkjuerpnwrlxavpj4r.jpg",
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/wvkbivieoqjjkasryout.jpg",
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/mccladadyiumz1ubbgzb.jpg",
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/m3tr4faovoamtuyi5ix6.jpg",
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/wftmrv6q8sgijfjujtmq.jpg",
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/mlggdl74y5v2egxcj4pg.jpg",
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/entobfvtputnk0rc0awn.jpg",
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/amytmlkazbbvshufl92u.jpg",
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/ugzgsry5yh1ggbav2a68.jpg",
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/zpbiylsdtvlao4ywonwi.jpg",
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/ff9s2o6oaqsahqqwb8yo.jpg",
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/vvwlu4bolhxdgpgujuks.jpg",
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/qj72upksxepv0c5j6zrc.jpg",
+              "http://res.cloudinary.com/dxgn2rpkt/image/upload/v1478113884/mb4akikky4flrazsniyv.jpg"
              ]
   imgLink = imgLinks[Random.rand(imgLinks.length)]
   poster = User.find(Random.rand(User.all.length)+1)
@@ -55,4 +68,11 @@ end
   comment = Comment.create(author_name: author.user_name,
                             author_id: author.id, image_id: imageId,
                             body: Faker::Hipster.sentence(sentenceLength, true, sentenceLength))
+end
+
+# create likes
+1000.times do
+  userId = Random.rand(User.all.length)+1
+  imgId = Random.rand(Image.all.length)+1
+  Like.create(author_id: userId, image_id: imgId)
 end
